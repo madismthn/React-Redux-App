73fb735a6fe3248995f5cf3fc3b2922a0e41d125
Madi <117897754+madismthn@users.noreply.github.com>
2024-06-10 17:51:32 +0300

    m

--- vite.config.js
+++ vite.config.js
@@ -5,19 +5,19 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/React-Redux-App/",
   plugins: [react()],
-  // build: {
-  //   rollupOptions: {
-  //     output: {
-  //       manualChunks(id) {
-  //         if (id.includes("node_modules")) {
-  //           return id
-  //             .toString()
-  //             .split("node_modules/")[1]
-  //             .split("/")[0]
-  //             .toString();
-  //         }
-  //       },
-  //     },
-  //   },
-  // },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
 });
