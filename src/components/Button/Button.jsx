b39e7f18641f98957a6bf733f08f444a2ea89df7
Madi <117897754+madismthn@users.noreply.github.com>
2024-06-13 10:24:34 +0300

    build the app on a new branch

--- src/components/Button/Button.jsx
+++
@@ -1,9 +0,0 @@
-import s from "./style.module.css";
-
-export const Button = ({ children, onClick }) => {
-  return (
-    <button className={s.button} onClick={onClick}>
-      {children}
-    </button>
-  );
-};
